3bf1510a995512c9e872dc498d4420938c27ecfc
Amanbtcse050 <bt26cse050@iiitn.ac.in>
2026-09-10 10:23:41 +0530

    Update ex6.c

--- ex6.c
+++ ex6.c
@@ -1,26 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
     int mrx;
-    printf("enter the marks\n");
-    scanf("%d", &mrx);
-    printf("grades\n");
 
-    if(mrx>=90)
+    printf("Enter the marks: ");
+    scanf("%d", &mrx);
+
+    printf("Grade: ");
+
+    if (mrx < 0 || mrx > 100)
+        printf("Invalid marks");
+    else if (mrx >= 90)
         printf("EX");
-    else if(mrx>=80)
+    else if (mrx >= 80)
         printf("A");
-    else if(mrx>=70)
+    else if (mrx >= 70)
         printf("B");
-    else if(mrx>=60)
+    else if (mrx >= 60)
         printf("C");
-    else if(mrx>=50)
+    else if (mrx >= 50)
         printf("D");
-    else if(mrx>=35)
+    else if (mrx >= 35)
         printf("P");
     else
-        printf("fail");
-
+        printf("Fail");
 
     return 0;
 }
