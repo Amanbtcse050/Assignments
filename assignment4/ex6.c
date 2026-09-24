eccc178c9004b06a2818b66b86e2c9da7860b3a6
Amanbtcse050 <bt26cse050@iiitn.ac.in>
2026-09-24 20:34:40 +0530

    Create ex6.c

---
+++ assignment4/ex6.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    float x, y;
+
+    
+    printf("Enter the coordinates (X and Y): ");
+    scanf("%f %f", &x, &y);
+
+    
+    if (x > 0 && y > 0) {
+        printf("The coordinate point (%.2f, %.2f) lies in the First Quadrant.\n", x, y);
+    } 
+    else if (x < 0 && y > 0) {
+        printf("The coordinate point (%.2f, %.2f) lies in the Second Quadrant.\n", x, y);
+    } 
+    else if (x < 0 && y < 0) {
+        printf("The coordinate point (%.2f, %.2f) lies in the Third Quadrant.\n", x, y);
+    } 
+    else if (x > 0 && y < 0) {
+        printf("The coordinate point (%.2f, %.2f) lies in the Fourth Quadrant.\n", x, y);
+    } 
+    else if (x == 0 && y == 0) {
+        printf("The coordinate point (%.2f, %.2f) is at the Origin.\n", x, y);
+    } 
+    else if (x == 0) {
+        printf("The coordinate point (%.2f, %.2f) lies on the Y-axis.\n", x, y);
+    } 
+    else {
+        printf("The coordinate point (%.2f, %.2f) lies on the X-axis.\n", x, y);
+    }
+
+    return 0;
+}
+
