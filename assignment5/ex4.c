095c8fa8b04493432a4474c07d3b9a89b4efc35f
Amanbtcse050 <bt26cse050@iiitn.ac.in>
2026-09-25 09:58:31 +0000

    uu

--- assignment5/ex4.c
+++ assignment5/ex4.c
@@ -2,7 +2,26 @@
 
 int main() {
     int n;
-    printf("enter the size of aray");
+    printf("enter the size of aray\n");
     scanf("%d", &n);
     int arr[n];
+    printf("enter the elements\n");
+
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    int larg = arr[0];
+    int slarg = -9999;
+    for(int i=0;i<n;i++){
+        if(larg<arr[i])
+            larg = arr[i];        
+    }
+    for(int i=0;i<n;i++){
+
+        if(slarg<arr[i]&&slarg!=larg)
+            slarg = arr[i];
+        
+    }
+    printf("%d ", larg);
+    printf("%d", slarg);
 }
